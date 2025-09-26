4fcd6b21b65d5c41a949353f9e7aeff84ba56403
Rizki Cahya <rizki.cahya03@gmail.com>
2025-09-26 09:41:51 +0700

    Update: variable & tipe data projects

--- 2 - Variable dan Tipe Data/variable.go
+++ 2 - Variable dan Tipe Data/variable.go
@@ -11,8 +11,12 @@ func main() {
 	kota := "Yogyakarta"
 	tahun := 2025
 
+	// const (nilai yang tidak dapat diubah)
+	const phi = 3.14
+
 	fmt.Println("Nama: ", nama)
 	fmt.Println("Umur: ", umur)
 	fmt.Println("Kota: ", kota)
 	fmt.Println("Tahun: ", tahun)
+	fmt.Println("Nilai Phi: ", phi)
 }
